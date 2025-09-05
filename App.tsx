93e3c705bffa181a3147a56a32b78ab463c9cd0e
Isadoracmb-09 <isacmb11@gmail.com>
2025-09-05 12:18:02 -0300

    kakakaka
    uhul

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
-import { Siglnin } from "./src/screens/siglnin";
-
+import {View, Text} from "react-native"
 
 export default function App() {
-  return (
-   <Siglnin/>
+return(
+<View style={{flex:1,justifyContent: 'center',alignItems:}}>
+  npx 
+  <Text> arrasou </Text>
+</View>
 
-  );}
+)
+
+
+  
+}
    
