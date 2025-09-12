a135339233cc3fce88902674fb8f66b894462a8b
Isadoracmb-09 <isacmb11@gmail.com>
2025-09-12 12:15:46 -0300

    kskkskskkskkskslalala
    lalalla

--- src/screens/siglnin/index.tsx
+++ src/screens/siglnin/index.tsx
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export function siglnin() {
+
+  const [text, SetText] = useState("")
   return (
     <View style={styles.container}>
       <Text>Testando a componentização</Text>
+      <TextInput style={{width:200, height:50, borderWidth:2}}/>
+      <Text>
+        Esse componente é show: {text}</Text>
       <StatusBar style="auto" />
     </View>
   );
