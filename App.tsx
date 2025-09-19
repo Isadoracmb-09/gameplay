51af3b63750b7a5080395cfada222e541bbf0f47
Isadoracmb-09 <isacmb11@gmail.com>
2025-09-19 12:16:44 -0300

    kskskskks

--- App.tsx
+++ App.tsx
@@ -2,9 +2,9 @@ import {View, Text} from "react-native"
 
 export default function App() {
 return(
-<View style={{flex:1,justifyContent: 'center',alignItems:}}>
+<View style={{flex:1,justifyContent: 'center',alignItems:"center"}}>
+ <Text> arrasou </Text>
  
-  <Text> arrasou </Text>
 </View>
 
 )
--- src/screens/siglnin/index.tsx
+++ src/screens/siglnin/index.tsx
@@ -1,26 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
 
+import {View, Image} from 'react-native';
+import illustrationpink from "../../assets/illustration.png"
 export function siglnin() {
 
-  const [text, SetText] = useState("")
+  
   return (
-    <View style={styles.container}>
-      <Text>Testando a componentização</Text>
-      <TextInput style={{width:200, height:50, borderWidth:2}}/>
-      <Text>
-        Esse componente é show: {text}</Text>
-      <StatusBar style="auto" />
+    <View >
+       <Image source={illustrationpink}/>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#4a0000ff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
