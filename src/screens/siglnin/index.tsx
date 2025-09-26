6faa427baf146c00916e844edd2d964e713e1642
Isadoracmb-09 <isacmb11@gmail.com>
2025-09-26 12:19:07 -0300

    qsdkfgkgkk

--- src/screens/siglnin/index.tsx
+++ src/screens/siglnin/index.tsx
@@ -1,12 +1,24 @@
 
-import {View, Image} from 'react-native';
+import {View, Image, Text} from 'react-native';
 import illustrationpink from "../../assets/illustration.png"
+import { stylesSignin } from './styles';
 export function siglnin() {
 
   
   return (
-    <View >
-       <Image source={illustrationpink}/>
+    <View  style={stylesSignin.container}>
+      <Image source={illustrationpink} style={stylesSignin.image} resizeMode="stretch"/>
+      <View style={stylesSignin.contet}> 
+        <Text style={stylesSignin.title}> 
+  Organize Suas {`\n`}
+  Jogatinas{'\n'}
+  facilmente
+          </Text>
+          <Text style={stylesSignin.subtitle} >
+crie grupos para jogar seus game {`\n`}
+favoritos com seus amigos
+        </Text>
+      </View>
     </View>
   )
 }
